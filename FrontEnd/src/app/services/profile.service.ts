27c9083f69d3d3a047bb24c317f6fd52b07a4018
vstecnic <vstecnic@gmail.com>
2025-05-09 00:46:14 -0300

    implementación de dashboard v1.0

--- FrontEnd/src/app/services/profile.service.ts
+++ FrontEnd/src/app/services/profile.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private apiUrl = 'https://dreamtravel.pythonanywhere.com/api/v1/profiles/me/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Obtener perfil
+  getProfile(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  // Actualizar perfil (ejemplo adicional)
+  updateProfile(data: any): Observable<any> {
+    return this.http.put(this.apiUrl, data);
+  }
 }
